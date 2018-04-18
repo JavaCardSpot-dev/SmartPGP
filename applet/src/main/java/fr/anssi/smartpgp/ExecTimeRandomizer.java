5dd3d4b9f55b8e7d9e226f57a3cacc5e3c7388d4
bencikpeter <bencik.peter.ml@gmail.com>
2018-04-18 16:36:18 +0200

    int -> short error fixed

--- applet/src/main/java/fr/anssi/smartpgp/ExecTimeRandomizer.java
+++ applet/src/main/java/fr/anssi/smartpgp/ExecTimeRandomizer.java
@@ -14,6 +14,6 @@ public class ExecTimeRandomizer {
 
         //extremely naive implementation, demonstrative purposes
         //TODO use more sophisticated approach
-        for (short i = 0; i < 100000; ++i){}
+        for (short i = (short) 0; i < (short) 100000; ++i){}
     }
 }
